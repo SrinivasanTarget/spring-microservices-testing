1212daea5a7a596e8f06256f122583072c1e04bb
Srinivasan Sekar <srinivasan.sekar1990@gmail.com>
2020-11-21 17:05:49 +0530

    update employee service test

--- employee-service/src/test/java/com/sample/EmployeeServiceTest.java
+++ employee-service/src/test/java/com/sample/EmployeeServiceTest.java
@@ -2,6 +2,10 @@ package com.sample;
 
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -13,43 +17,50 @@ import static org.mockito.Mockito.*;
 class EmployeeServiceTest {
 
     private final EmployeeRepository employeeRepository = mock(EmployeeRepo.class);
-    private final EmployeeService employeeService = new EmployeeService(employeeRepository);
+    private final RestTemplate restTemplate = mock(RestTemplate.class);
+    private final EmployeeService employeeService = new EmployeeService(employeeRepository, restTemplate);
 
     @Test
-    void shouldCallDaoToSaveEmployee() {
-        Employee actualEmployee = new Employee(1L, 101L, "srini", 18);
+    void shouldBeAbleToSaveEmployeeSuccessfully() {
+        Employee actualEmployee = Employee.builder().id(1L).name("srini").age(18).build();
         when(employeeRepository.save(actualEmployee)).thenReturn(actualEmployee);
+        DepartmentResponse departmentResponse = DepartmentResponse.builder().id(101L).name("CSC").build();
+        ReflectionTestUtils.setField(employeeService, "departmentServiceURL", "http://localhost:8081");
+        when(restTemplate.getForEntity("http://localhost:8081/1", DepartmentResponse.class))
+          .thenReturn(new ResponseEntity<>(departmentResponse, HttpStatus.OK));
 
-        Employee employee = employeeService.save(actualEmployee);
+        Employee employee = employeeService.saveEmployee(actualEmployee);
 
         ArgumentCaptor<Employee> argumentCaptor = ArgumentCaptor.forClass(Employee.class);
         verify(employeeRepository).save(argumentCaptor.capture());
         Employee expectedEmployee = argumentCaptor.getValue();
         assertThat(expectedEmployee.getName()).isEqualTo("srini");
         assertThat(expectedEmployee.getAge()).isEqualTo(18);
+        assertThat(expectedEmployee.getDepartmentId()).isEqualTo(101);
         assertThat(employee.getName()).isEqualTo("srini");
         assertThat(employee.getAge()).isEqualTo(18);
+        assertThat(employee.getDepartmentId()).isEqualTo(101);
     }
 
     @Test
     void shouldGetAllEmployeesFromRepository() {
         ArrayList<Employee> expectedEmployees = new ArrayList<>();
-        expectedEmployees.add(new Employee(1L, 101L, "srini", 18));
+        expectedEmployees.add(Employee.builder().id(1L).name("srini").age(18).build());
         when(employeeRepository.findAll()).thenReturn(expectedEmployees);
 
-        Collection<Employee> actualEmployees = employeeService.findAll();
+        Collection<Employee> actualEmployees = employeeService.findAllEmployees();
 
         assertThat(actualEmployees).isEqualTo(expectedEmployees);
     }
 
     @Test
     void shouldGetEmployeeFromRepository() {
-        Employee expectedEmployee = new Employee(1L, 101L, "srini", 18);
+        Employee expectedEmployee = Employee.builder().id(1L).name("srini").age(18).build();
         when(employeeRepository.findById(1L)).thenReturn(Optional.of(expectedEmployee));
 
-        Optional<Employee> actualEmployee = employeeService.findById(1L);
+        Employee actualEmployee = employeeService.findByEmployeeId(1L);
 
-        assertThat(actualEmployee).isEqualTo(Optional.of(expectedEmployee));
+        assertThat(actualEmployee).isEqualTo(expectedEmployee);
     }
 
 }
